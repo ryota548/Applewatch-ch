6c78e0f3f406d649e6ac0d69b09cfceaef360f76
小芝　涼太　 <ryota_koshiba@rkoshiba.local>
2015-08-26 12:11:16 +0900

    プロジェクトの追加

---
+++ AppleWatch-ch/AppleWatch-ch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppleWatch-ch
+//
+//  Created by 小芝　涼太　 on 2015/08/26.
+//  Copyright (c) 2015年 ryota_koshiba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
