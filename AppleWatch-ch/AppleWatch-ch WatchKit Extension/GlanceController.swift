7d98342fe7637fc8ef7a749192f0eee7af674192
小芝　涼太　 <ryota_koshiba@rkoshiba.local>
2015-08-27 12:15:32 +0900

    chapter6　完成

--- AppleWatch-ch/AppleWatch-ch WatchKit Extension/GlanceController.swift
+++ AppleWatch-ch/AppleWatch-ch WatchKit Extension/GlanceController.swift
@@ -11,6 +11,10 @@ import Foundation
 
 
 class GlanceController: WKInterfaceController {
+    
+    @IBOutlet var label: WKInterfaceLabel!
+    @IBOutlet var image: WKInterfaceImage!
+    
 
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
@@ -21,10 +25,82 @@ class GlanceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        // 現在の状態を復帰する
+        // 植物の状態
+        var status: InterfaceController.PlantStatus?
+        = InterfaceController.PlantStatus.Wakaba
+        var usrDef: NSUserDefaults = NSUserDefaults()
+        var st: Int? = usrDef.integerForKey("STATUS")
+        if st != nil {
+            status = InterfaceController.PlantStatus(rawValue: st!)
+            if status == nil {
+                status = InterfaceController.PlantStatus.Wakaba
+            }
+        }
+        // スコア
+        var score: Int? = 0
+        var sc: Int? = usrDef.integerForKey("SCORE")
+        if sc != nil {
+            score = sc!
+        }
+        var numFluit: Int? = 0
+        var fl: Int? = usrDef.integerForKey("FLUIT")
+        if fl != nil {
+            numFluit = fl!
+        }
+        
+        // 状態を表示する
+        switch status!
+        {
+        case .Wakaba:
+            image.setImageNamed("WAKABA")
+        case .Totyuu:
+            image.setImageNamed("TOTYUU")
+        case .Kareki:
+            image.setImageNamed("KAREKI")
+        case .Seiboku:
+            // 画像を読み込む
+            var img1: UIImage? = UIImage(named:"SEIBOKU")
+            var img2: UIImage? = UIImage(named:"FLUIT")
+            // サイズを取得
+            var size: CGSize? = img1?.size
+            
+            if size != nil {
+                // 描写を開始
+                UIGraphicsBeginImageContext(size!)
+                
+                // 背景に成木を描写
+                var rect: CGRect = CGRectMake(0, 0, size!.width, size!.height)
+                img1?.drawInRect(rect)
+                // 果物を描写する場所
+                var x: CGFloat = 0
+                var y: CGFloat = 0
+                // 果物の数だけ描写する
+                for var n: Int = 1; n <= numFluit; n++ {
+                    // 現在の場所に描写
+                    var point: CGPoint = CGPointMake(x, y)
+                    img2?.drawAtPoint(point)
+                    // 描写する場所をずらす
+                    x += size!.width / 4
+                    if n > 0 && n % 4 == 0 {
+                        // 一段につき4個描写し次の段へ
+                        x = 0
+                        y += size!.height / 3
+                    }
+                }
+                // 描写結果を取得
+                var img: UIImage? = UIGraphicsGetImageFromCurrentImageContext()
+                // 描写結果を表示
+                image.setImage(img)
+                // 描写を終了
+                UIGraphicsEndImageContext()
+            }
+        }
+        // スコアを表示する
+        label.setText("Score:\(score!)")
     }
-
+    
     override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
 
